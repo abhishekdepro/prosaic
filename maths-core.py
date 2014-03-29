d9b59cf5c569d63d0713e4af1c1259a8ac0df6e2
Abhishek De <abhishekde@hotmail.com>
2014-03-29 14:35:40 +0530

    Added improved number of divisors method

--- maths-core.py
+++ maths-core.py
@@ -1,36 +1,50 @@
-def divisor_calculator(num):
-	_num=num
-	_div=[]
-	_div.append(1)
-	for i in range(2, num-1):
-		if(num%i==0):
-			num=num/i
-			_div.append(i)
-			i=i-1
-	_p=[1]
-	for i in range(0, len(_div)):
-		q=_div[i]
-		for j in range(i+1, len(_div)):
-			_p.append(_div[j]*q)
-	_p.append(_num)
-	return _p
+import operator
+# A slightly efficient superset of primes.
+def PrimesPlus():
+  yield 2
+  yield 3
+  i = 5
+  while True:
+    yield i
+    if i % 6 == 1:
+      i += 2
+    i += 2
+# Returns a dict d with n = product p ^ d[p]
+def GetPrimeDecomp(n):
+  d = {}
+  fact=[]
+  primes = PrimesPlus()
+  for p in primes:
+    while n % p == 0:
+      n /= p
+      d[p] = d.setdefault(p, 0) + 1
+      fact.append(p)
+    if n == 1:
+      print d
+      print fact
+      return d
+def NumberOfDivisors(n):
+  d = GetPrimeDecomp(n)
+  powers_plus = map(lambda x: x+1, d.values())
+  print powers_plus
+  return reduce(operator.mul, powers_plus, 1)
 
 def primegenerator(limit):
-	_set=[]
-	for i in range(2,limit):
-		if(isPrime(i)==True):
-			_set.append(i)
-	return _set
+    _set=[]
+    for i in range(2,limit):
+        if(isPrime(i)==True):
+            _set.append(i)
+    return _set
 
 def isPrime(num):
-	flag=0
-	for i in range(2, num-1):
-		if num%i==0:
-			flag=1
-			break
-		else:
-			continue
-	if flag==1:
-		return False
-	else:
-		return True
+    flag=0
+    for i in range(2, num-1):
+        if num%i==0:
+            flag=1
+            break
+        else:
+            continue
+    if flag==1:
+        return False
+    else:
+        return True
